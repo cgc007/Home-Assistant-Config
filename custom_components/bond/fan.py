5387668337e188448506b3d67e73d5341004ad1c
cgc007 <cgc007@gmail.com>
2020-06-29 01:26:03 +0100

    New Pi Build

---
+++ custom_components/bond/fan.py
@@ -0,0 +1,145 @@
+"""Bond Home Fan Integration"""
+from homeassistant.components.fan import (
+    SUPPORT_SET_SPEED,
+    SPEED_OFF,
+    SPEED_LOW,
+    SPEED_MEDIUM,
+    SPEED_HIGH,
+    FanEntity
+)
+
+from bond import (
+    BOND_DEVICE_TYPE_CEILING_FAN,
+    BOND_DEVICE_ACTION_SET_SPEED
+)
+
+import logging
+DOMAIN = 'bond'
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up the Bond Fan platform"""
+    bond = hass.data[DOMAIN]['bond_hub']
+
+    for deviceId in bond.getDeviceIds():
+        device = bond.getDevice(deviceId)
+        if device['type'] != BOND_DEVICE_TYPE_CEILING_FAN:
+            continue
+
+        deviceProperties = bond.getProperties(deviceId)
+        fan = BondFan(bond, deviceId, device, deviceProperties)
+        add_entities([fan])
+
+
+class BondFan(FanEntity):
+    """Representation of an Bond Fan"""
+
+    def __init__(self, bond, deviceId, device, properties):
+        """Initialize a Bond Fan"""
+        self._bond = bond
+        self._deviceId = deviceId
+        self._device = device
+        self._properties = properties
+        self._name = device['name']
+        self._state = None
+        self._speed_list = []
+        self._speed_name_by_value = {}
+        self._attributes = {}
+
+        if BOND_DEVICE_ACTION_SET_SPEED in self._device['actions']:
+            if 'max_speed' in self._properties:
+                self._speed_high = int(self._properties['max_speed'])
+                self._speed_low = int(1)
+                self._speed_list.append(SPEED_LOW)
+                self._speed_name_by_value[self._speed_low] = SPEED_LOW
+                if self._speed_high > 2:
+                    self._speed_medium = (self._speed_high + 1) // 2
+                    self._speed_list.append(SPEED_MEDIUM)
+                    self._speed_name_by_value[self._speed_medium] = SPEED_MEDIUM
+                self._speed_list.append(SPEED_HIGH)
+                self._speed_name_by_value[self._speed_high] = SPEED_HIGH
+
+    @property
+    def name(self):
+        """Return the display name of this fan"""
+        return self._name
+
+    @property
+    def is_on(self):
+        """Return true if fan is on"""
+        return self._state
+
+    @property
+    def speed_list(self) -> list:
+        """Get the list of available speeds."""
+        return self._speed_list
+    
+    @property
+    def device_state_attributes(self):
+        """Return state attributes """
+        """For now, the only attribute being tracked is 'current speed'.
+        Since this is accessible via the 'speed' property, there is no
+        need to return any attributes at this time.
+        """
+        # return self._attributes
+        return None
+
+    @property
+    def supported_features(self):
+        """Flag supported features."""
+        supported_features = 0
+
+        if BOND_DEVICE_ACTION_SET_SPEED in self._device['actions']:
+            supported_features |= SUPPORT_SET_SPEED
+
+        return supported_features
+
+    def turn_on(self, speed=None, **kwargs):
+        """Instruct the fan to turn on"""
+        self._bond.turnOn(self._deviceId)
+        if speed is not None:
+            self.set_speed(speed)
+
+    def turn_off(self, **kwargs):
+        """Instruct the fan to turn off"""
+        self._attributes['current_speed'] = SPEED_OFF
+        self._bond.turnOff(self._deviceId)
+
+    def set_speed(self, speed: str) -> None:
+        """Set the speed of the fan."""
+        if speed == SPEED_HIGH:
+            self._bond.setSpeed(self._deviceId, self._speed_high)
+        elif speed == SPEED_MEDIUM:
+            self._bond.setSpeed(self._deviceId, self._speed_medium)
+        elif speed == SPEED_LOW:
+            self._bond.setSpeed(self._deviceId, self._speed_low)
+        self._attributes['current_speed'] = speed
+
+    @property
+    def speed(self) -> str:
+        """Return the current speed."""
+        return self._attributes.get("current_speed")
+
+    def update(self):
+        """Fetch new state data for this fan
+        This is the only method that should fetch new data for Home Assistant
+        """
+        bondState = self._bond.getDeviceState(self._deviceId)
+        if 'power' in bondState:
+            self._state = True if bondState['power'] == 1 else False
+            if self._state and bondState['speed'] in self._speed_name_by_value:
+                self._attributes['current_speed'] = self._speed_name_by_value[bondState['speed']]
+            else:
+                self._attributes['current_speed'] = SPEED_OFF
+
+    @property
+    def unique_id(self):
+        """Get the unique identifier of the device."""
+        return self._deviceId
+
+    @property
+    def device_id(self):
+        """Return the ID of this fan."""
+        return self.unique_id
---
+++ custom_components/bond/light.py
@@ -0,0 +1,217 @@
+"""Bond Home Light Integration"""
+from homeassistant.components.light import (
+    ATTR_BRIGHTNESS,
+    PLATFORM_SCHEMA,
+    SUPPORT_BRIGHTNESS,
+    LightEntity
+)
+
+from bond import (
+    BOND_DEVICE_TYPE_CEILING_FAN,
+    BOND_DEVICE_TYPE_FIREPLACE,
+    BOND_DEVICE_ACTION_TURN_LIGHT_ON,
+    BOND_DEVICE_ACTION_TURN_LIGHT_OFF,
+    BOND_DEVICE_ACTION_TURN_ON,
+    BOND_DEVICE_ACTION_TURN_OFF,
+    BOND_DEVICE_ACTION_TOGGLE_LIGHT,
+    BOND_DEVICE_ACTION_TOGGLE_POWER,
+    BOND_DEVICE_ACTION_SET_FLAME,
+    BOND_DEVICE_ACTION_INCREASE_FLAME,
+    BOND_DEVICE_ACTION_DECREASE_FLAME
+)
+
+import logging
+DOMAIN = 'bond'
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up the Bond Light platform."""
+    # Setup connection with devices/cloud
+    bond = hass.data[DOMAIN]['bond_hub']
+
+    # Add devices
+    for deviceId in bond.getDeviceIds():
+        device = bond.getDevice(deviceId)
+        deviceType = device['type']
+        actions = device['actions']
+
+        if deviceType == BOND_DEVICE_TYPE_CEILING_FAN:
+            # If the device type is not Ceiling Fan, or it is
+            # Ceiling Fan but has no action for light control
+            # then don't create a light instance.
+            supportsLightActions = \
+                BOND_DEVICE_ACTION_TURN_LIGHT_ON in actions or \
+                BOND_DEVICE_ACTION_TURN_LIGHT_OFF in actions or \
+                BOND_DEVICE_ACTION_TOGGLE_LIGHT in actions
+            if supportsLightActions:
+                deviceProperties = bond.getProperties(deviceId)
+                light = BondLight(bond, deviceId, device, deviceProperties)
+                add_entities([light])
+
+        elif deviceType == BOND_DEVICE_TYPE_FIREPLACE:
+            supportsFlameActions = \
+                BOND_DEVICE_ACTION_SET_FLAME in actions or \
+                BOND_DEVICE_ACTION_INCREASE_FLAME in actions or \
+                BOND_DEVICE_ACTION_DECREASE_FLAME in actions
+
+            supportsGenericActions = \
+                BOND_DEVICE_ACTION_TURN_ON in actions or \
+                BOND_DEVICE_ACTION_TURN_OFF in actions or \
+                BOND_DEVICE_ACTION_TOGGLE_POWER in actions
+
+            if supportsGenericActions:
+                deviceProperties = bond.getProperties(deviceId)
+                fireplace = BondFireplace(bond,
+                                          deviceId,
+                                          device,
+                                          deviceProperties,
+                                          supportsFlameActions)
+                add_entities([fireplace])
+
+
+class BondLight(LightEntity):
+    """Representation of an Bond Light."""
+
+    def __init__(self, bond, deviceId, device, properties):
+        """Initialize a Bond Light."""
+        self._bond = bond
+        self._deviceId = deviceId
+        self._device = device
+        self._properties = properties
+        self._name = device['name']
+        self._state = None
+
+    @property
+    def name(self):
+        """Return the display name of this light."""
+        return self._name
+
+    @property
+    def is_on(self):
+        """Return true if light is on."""
+        return self._state
+
+    def turn_on(self, **kwargs):
+        """Instruct the light to turn on."""
+        self._bond.turnLightOn(self._deviceId)
+
+    def turn_off(self, **kwargs):
+        """Instruct the light to turn off."""
+        self._bond.turnLightOff(self._deviceId)
+
+    def update(self):
+        """Fetch new state data for this light.
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        bondState = self._bond.getDeviceState(self._deviceId)
+        if 'light' in bondState:
+            self._state = True if bondState['light'] == 1 else False
+
+    @property
+    def unique_id(self):
+        """Get the unique identifier of the device."""
+        return self._deviceId
+
+    @property
+    def device_id(self):
+        """Return the ID of this light."""
+        return self.unique_id
+
+
+class BondFireplace(LightEntity):
+    """Representation of an Bond Fireplace."""
+
+    def __init__(self, bond, deviceId, device, properties, supportsFlame):
+        """Initialize a Bond Fireplace."""
+        self._bond = bond
+        self._deviceId = deviceId
+        self._device = device
+        self._properties = properties
+        name = "Fireplace" if "name" not in properties else properties['name']
+        if "location" in properties:
+            self._name = f"{properties['location']} {name}"
+        else:
+            self._name = name
+        self._supportsFlameAction = supportsFlame
+        self._last_flame = 255
+        self._flame = None
+        self._state = None
+
+    @property
+    def name(self):
+        """Return the display name of this light."""
+        return self._name
+
+    @property
+    def supported_features(self):
+        """Flag supported features.
+        This method is optional. Removing it indicates to Home Assistant
+        that brightness is not supported for this light.
+        """
+        return SUPPORT_BRIGHTNESS if self._supportsFlameAction else 0
+
+    @property
+    def brightness(self):
+        """Return the brightness of the light.
+
+        This method is optional. Removing it indicates to Home Assistant
+        that brightness is not supported for this light.
+        """
+        return self._flame
+
+    @property
+    def is_on(self):
+        """Return true if light is on."""
+        return self._state
+
+    def turn_on(self, **kwargs):
+        """Instruct the fireplace to turn on.
+
+        You can skip the brightness part if your light does not support
+        brightness control.
+        """
+        self._bond.turnOn(self._deviceId)
+
+        if self._supportsFlameAction:
+            # Convert Home Assistant flame level (0-255) to bond level (0-100)
+            brightness = kwargs.get(ATTR_BRIGHTNESS, self._last_flame)
+            flame = int((brightness * 100) / 255)
+
+            # NOTE: We may need to call set flame down and set flame up.
+            self._bond.setFlame(self._deviceId, flame)
+            self._flame = flame
+
+    def turn_off(self, **kwargs):
+        """Instruct the fireplace to turn off."""
+        self._bond.turnOff(self._deviceId)
+        if self._flame:
+            self._last_flame = self._flame
+
+    def update(self):
+        """Fetch new state data for this light.
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        bondState = self._bond.getDeviceState(self._deviceId)
+        if 'power' in bondState:
+            self._state = True if bondState['power'] == 1 else False
+
+        # Convert bond level (0-100) to Home Assistant flame level (0-255)
+        if 'flame' in bondState:
+            self._flame = int((bondState['flame'] * 255) / 100)
+
+    @property
+    def unique_id(self):
+        """Get the unique identifier of the device."""
+        return self._deviceId
+
+    @property
+    def device_id(self):
+        """Return the ID of this light."""
+        return self.unique_id
+
+    @property
+    def icon(self):
+        """Get the icon to use in the front end."""
+        return "mdi:fireplace" if self._state is True else "mdi:fireplace-off"
